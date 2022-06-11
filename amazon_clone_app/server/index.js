5d49573ec2807e98a9c97a586c2bad32a191eec3
Mahesh0267 <106401664+Mahesh0267@users.noreply.github.com>
2022-06-11 19:11:04 +0530

    created mongoose and expess connection for api

--- amazon_clone_app/server/index.js
+++ amazon_clone_app/server/index.js
@@ -1,15 +1,23 @@
-console.log("Hello World....");
-//print("hello world")
 const express = require('express');
+const mongoose = require("mongoose");
+const authRouter = require('./routes/auth');
+
+
 const PORT = 3000;
-
 const app = express();
+const DB ="mongodb+srv://mahesh:mahesh95@cluster0.mgjdfuq.mongodb.net/?retryWrites=true&w=majority";
 
-//creating an api
-//CRUD request
+app.use(authRouter);
 
-app.listen(PORT,"0.0.0.0",() =>{
-    console.log(`connected  at port  ${PORT}`);
+mongoose
+  .connect(DB)
+  .then(() => {
+    console.log("Connection Successful");
+  })
+  .catch((e) => {
+    console.log(e);
+  })
+
+app.listen(PORT, () => {
+  console.log(`connected  at port  ${PORT}`);
 });
-//localhost
-
